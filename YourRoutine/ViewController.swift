3cd1b3a5f03d5c2a821cd6287c3ca73782f7ec33
Gimblet <gimbletdev@gmail.com>
2025-12-15 15:56:41 -0500

    Added data to the label that shows how much progress youve made

--- YourRoutine/ViewController.swift
+++ YourRoutine/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        actualizarCampos()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -27,18 +28,57 @@ class ViewController: UIViewController {
     
     func actualizarCampos() {
         if let currentRoutine = RutinaDAO().findCurrent() {
-            let hasta = "Hasta las \(currentRoutine.fin!)"
-            btnRutinaHasta.configuration?.title = "\(hasta)"
+            let until = "Hasta las \(currentRoutine.fin!)"
+            let currentMin:String = String(getCurrentMin(rutina: currentRoutine))
+            let totalMin:String = String(getTotalMin(rutina: currentRoutine))
+            
+            lblProgresoMin.text = "\(currentMin)/\(totalMin) min"
+            btnRutinaHasta.setTitle(until, for: .normal)
             lblRutinaTitulo.text = currentRoutine.nombre.unsafelyUnwrapped
         } else {
-            lblRutinaActual.text = "Sin Rutina Actual"
+            lblProgresoMin.text = "..."
+            lblRutinaActual.text = "Sin Rutina"
             btnRutinaHasta.setTitle("Estas libre ;)", for: .normal)
             lblRutinaTitulo.text = "Descansa, te lo mereces..."
         }
     }
     
-    func obtenerProgresoMin() {
+    func getCurrentMin(rutina:RutinaEntity) -> Int {
+        let today = Date()
+        let hour = Int((Calendar.current.component(.hour, from: today)))
+        let min = Int((Calendar.current.component(.minute, from: today)))
         
+        let arrayTiempoInicio:[String] = rutina.inicio!.components(separatedBy: ":")
+        var arrayNumeroInicio:[Int] = []
+        
+        for t in arrayTiempoInicio {
+            arrayNumeroInicio.append(Int(t).unsafelyUnwrapped)
+        }
+        
+        let totalTiempoInicio = ((arrayNumeroInicio[0] * 60) + arrayNumeroInicio[1])
+        
+        return (((hour * 60) + min) - totalTiempoInicio)
+    }
+    
+    func getTotalMin(rutina:RutinaEntity) -> Int {
+        let arrayTiempoInicio:[String] = rutina.inicio!.components(separatedBy: ":")
+        let arrayTiempoFin:[String] = rutina.fin!.components(separatedBy: ":")
+        
+        var arrayNumeroInicio:[Int] = []
+        var arrayNumeroFin:[Int] = []
+
+        for t in arrayTiempoInicio {
+            arrayNumeroInicio.append(Int(t).unsafelyUnwrapped)
+        }
+        
+        for t in arrayTiempoFin {
+            arrayNumeroFin.append(Int(t).unsafelyUnwrapped)
+        }
+        
+        let totalTiempoInicio = ((arrayNumeroInicio[0] * 60) + arrayNumeroInicio[1])
+        let totalTiempoFin = ((arrayNumeroFin[0] * 60) + arrayNumeroFin[1])
+        
+        return totalTiempoFin - totalTiempoInicio
     }
 
 }
