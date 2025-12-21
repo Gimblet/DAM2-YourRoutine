1b69eef8bba58d134e93af6301384ef7b739a828
Gimblet <gimbletdev@gmail.com>
2025-12-21 14:51:47 -0500

    Fix: Fixed wrong background colors in edit routine dias cell

--- YourRoutine/celdas/DiaCell.swift
+++ YourRoutine/celdas/DiaCell.swift
@@ -12,34 +12,30 @@ class DiaCell: UICollectionViewCell {
     
     @IBAction func btnDia(_ sender: UIButton) {
         /*if(sender.isSelected) {
-            sender.isSelected = false
-            sender.setTitleColor(UIColor.black, for: .normal)
-            RutinaDAO().removeDiaTemporal(dia: sender.titleLabel?.text ?? "")
-        } else {
-            sender.isSelected = true
-            sender.setTitleColor(UIColor.white, for: .selected)
-            RutinaDAO().addDiaTemporal(etiqueta: sender.titleLabel?.text ?? "")
-        }*/
+         sender.isSelected = false
+         sender.setTitleColor(UIColor.black, for: .normal)
+         RutinaDAO().removeDiaTemporal(dia: sender.titleLabel?.text ?? "")
+         } else {
+         sender.isSelected = true
+         sender.setTitleColor(UIColor.white, for: .selected)
+         RutinaDAO().addDiaTemporal(etiqueta: sender.titleLabel?.text ?? "")
+         }*/
         
         
         guard let dia = sender.titleLabel?.text else { return }
         sender.isSelected.toggle()
-
+        
         sender.setTitleColor(.black, for: .normal)
         sender.setTitleColor(UIColor.white, for: .selected)
-
-        // Lógica de negocio        
+        
+        // Lógica de negocio
         RutinaDAO().toggleDia(dia)
     }
     
     func configurar(nombre: String, seleccionado: Bool) {
-            /*btnDia.setTitle(nombre, for: .normal)
-            btnDia.backgroundColor = seleccionado ? .systemBlue : .lightGray
-            btnDia.setTitleColor(.white, for: .normal)*/
-        
-            btnDia.setTitle(nombre, for: .normal)
-            btnDia.isSelected = seleccionado
-            btnDia.backgroundColor = seleccionado ? .systemBlue : .lightGray
-            btnDia.setTitleColor(.white, for: .normal)
-        }
+        btnDia.setTitle(nombre, for: .normal)
+        btnDia.isSelected = seleccionado
+        btnDia.setTitleColor(.black, for: .normal)
+        btnDia.setTitleColor(UIColor.white, for: .selected)
+    }
 }
